573ab39c8c97c0a76ab017291f3989d243b8d51b
whisperpine <yusong.lai@icloud.com>
2023-12-06 18:38:37 +0800

    add examples/fixed_timestep.rs

---
+++ examples/fixed_timestep.rs
@@ -0,0 +1,25 @@
+#![cfg_attr(debug_assertions, allow(unused))]
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("\n#### fixed_timestep ####\n");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Time::<Fixed>::from_seconds(0.5))
+        .add_systems(Update, (log_update, bevy::window::close_on_esc))
+        .add_systems(FixedUpdate, log_fixed_update)
+        .run()
+}
+
+fn log_update(mut previous: Local<f32>, time: Res<Time>) {
+    info!("log_update: {}", time.elapsed_seconds() - *previous);
+    *previous = time.elapsed_seconds();
+}
+
+fn log_fixed_update(mut previous: Local<f32>, time: Res<Time>, fixed_time: Res<Time<Fixed>>) {
+    info!("log_fixed_update: {}", time.elapsed_seconds() - *previous);
+    let overstep = fixed_time.overstep().as_secs_f32();
+    info!("overstep: {}", overstep);
+    *previous = time.elapsed_seconds();
+}
