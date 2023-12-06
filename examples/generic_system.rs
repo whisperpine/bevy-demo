23feb991be20139f7d5d695ccd9e4ceb7032e52a
whisperpine <yusong.lai@icloud.com>
2023-12-06 19:16:24 +0800

    add examples/generic_system.rs

---
+++ examples/generic_system.rs
@@ -0,0 +1,57 @@
+#![cfg_attr(debug_assertions, allow(unused))]
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("\n#### generic_system ####\n");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (bevy::window::close_on_esc, change_app_state, log_on_enter),
+        )
+        .add_systems(OnExit(AppState::Menu), cleanup_system::<MenuStateTag>)
+        .add_systems(OnExit(AppState::InGame), cleanup_system::<InGameStateTag>)
+        .run();
+}
+
+#[derive(Component)]
+struct MenuStateTag;
+
+#[derive(Component)]
+struct InGameStateTag;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash, States)]
+enum AppState {
+    #[default]
+    Menu,
+    InGame,
+}
+
+fn setup(mut cmd: Commands) {
+    cmd.spawn(MenuStateTag);
+    cmd.spawn(InGameStateTag);
+}
+
+fn change_app_state(mut app_state: ResMut<NextState<AppState>>, input: Res<Input<KeyCode>>) {
+    if input.just_pressed(KeyCode::Q) {
+        app_state.set(AppState::Menu);
+    } else if input.just_pressed(KeyCode::W) {
+        app_state.set(AppState::InGame);
+    }
+}
+
+fn log_on_enter(app_state: Res<State<AppState>>) {
+    if app_state.is_changed() {
+        info!("AppState: {:?}", app_state.get());
+    }
+}
+
+fn cleanup_system<T: Component>(mut cmd: Commands, query: Query<Entity, With<T>>) {
+    for entity in query.iter() {
+        info!("despawn {:?}", entity);
+        cmd.entity(entity).despawn_recursive();
+    }
+}
