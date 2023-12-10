7190fe9670cc426865af15fe8ba28c21f851b61c
whisperpine <yusong.lai@icloud.com>
2023-12-10 23:52:10 +0800

    add examples/run_conditions.rs

---
+++ examples/run_conditions.rs
@@ -0,0 +1,71 @@
+#![cfg_attr(debug_assertions, allow(unused))]
+
+use std::fmt::Debug;
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("\n#### run_conditions ####\n");
+
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_resource::<InputCounter>()
+        .add_systems(Update, (bevy::window::close_on_esc, log_input))
+        .add_systems(
+            Update,
+            count_input
+                .run_if(resource_exists::<InputCounter>())
+                .run_if(resource_exists::<Unused>().or_else(has_user_input)),
+        )
+        .add_systems(
+            Update,
+            print_input_counter.after(count_input).run_if(
+                resource_exists::<InputCounter>().and_then(|input_counter: Res<InputCounter>| {
+                    input_counter.is_changed() && !input_counter.is_added()
+                }),
+            ),
+        )
+        .add_systems(
+            Update,
+            print_time_message
+                .run_if(is_time_passed(2.))
+                .run_if(not(is_time_passed(2.1))),
+        )
+        .run();
+}
+
+#[derive(Resource, Debug, Default, Deref, DerefMut)]
+struct InputCounter(u32);
+
+#[derive(Resource)]
+struct Unused;
+
+fn count_input(mut input_counter: ResMut<InputCounter>) {
+    **input_counter += 1;
+}
+
+fn print_input_counter(input_counter: ResMut<InputCounter>) {
+    println!("{:?}", *input_counter);
+}
+
+fn log_input(keyboard: Res<Input<KeyCode>>, mouse: Res<Input<MouseButton>>) {
+    for key in keyboard.get_just_pressed() {
+        println!("KeyCode: {:?}", key);
+    }
+    for mouse in mouse.get_just_pressed() {
+        println!("MouseButton: {:?}", mouse);
+    }
+}
+
+fn has_user_input(keyboard: Res<Input<KeyCode>>, mouse: Res<Input<MouseButton>>) -> bool {
+    keyboard.just_pressed(KeyCode::Space)
+        || mouse.any_just_pressed([MouseButton::Left, MouseButton::Right])
+}
+
+fn print_time_message() {
+    println!("It has been more than 2 seconds since the program started and less than 2.1 seconds");
+}
+
+fn is_time_passed(cmp_time: f32) -> impl FnMut(Res<Time>) -> bool {
+    move |time: Res<Time>| time.elapsed_seconds() > cmp_time
+}
